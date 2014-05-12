45e5ffa16d2beef19a804d8b737671c6be009156
Eric Gaudet <egaudet@thefind.com>
2014-05-12 14:25:24 -0700

    class to create a string from other columns using a format string

---
+++ src/com/thefind/sisyphus/ColumnsFormat.java
@@ -0,0 +1,27 @@
+package com.thefind.sisyphus;
+
+import java.util.*;
+
+import com.thefind.util.StringUtil;
+
+/**
+ * @author Eric Gaudet
+ */
+public class ColumnsFormat
+extends Modifier
+{
+  private final String format_;
+
+  public ColumnsFormat(String outcol, String format, String... incol)
+  {
+    super(incol, new String[] { outcol });
+    format_ = format;
+  }
+
+  public void compute(String[] input, String[] result)
+  { result[0] = String.format(format_, (Object[]) input); }
+
+  @Override
+  public String toStringModif()
+  { return ".format('"+format_+"')"; }
+}
